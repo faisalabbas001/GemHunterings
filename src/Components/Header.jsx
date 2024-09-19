d3570e283d908f1173a26e96bd1a8c415374eb54
soban <faisalabbas7959@gmail.com>
2024-09-19 07:21:03 +0500

    update header file

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -17,7 +17,7 @@ const Header = () => {
         <div className="mt-4 block lg:hidden">
           {isConnected ? (
             <button
-              className="bg-white text-white text-[14px] sm:text-[16px] px-2 sm:px-4 py-[6px] transform scale-90 sm:scale-100 border border-white border-opacity-50 rounded-lg"
+              className="bg-[#363A41] text-white text-[14px] sm:text-[16px] px-2 sm:px-4 py-[6px] transform scale-90 sm:scale-100 border border-white border-opacity-50 rounded-lg"
               onClick={() => open()}
             >
               {address.slice(0, 4)}...{address.slice(-4)}
@@ -25,7 +25,7 @@ const Header = () => {
           ) : (
             <button
               onClick={() => open()}
-              className="bg-[#363A41] text-black text-[14px] sm:text-[16px] px-1 sm:px-3 py-1 transform scale-90 sm:scale-100 border border-white border-opacity-50 rounded-lg"
+              className="bg-[#363A41] text-white text-[14px] sm:text-[16px] px-1 sm:px-3 py-1 transform scale-90 sm:scale-100 border border-white border-opacity-50 rounded-lg"
             >
               Connect Wallet
             </button>
@@ -35,9 +35,12 @@ const Header = () => {
       {/* Desktop View (Hidden on smaller screens) */}
       <div className="fixed top-10 right-4 hidden lg:block cursor-pointer">
         {isConnected ? (
-          <div className="bg-white text-black py-1 px-4 rounded-2xl hover:bg-green-700" onClick={open}>
-            {address.slice(0, 6)}...{address.slice(-4)}
-          </div>
+          <button
+            className="bg-[#363A41] text-white text-[14px] sm:text-[16px] px-2 sm:px-4 py-[6px] transform scale-90 sm:scale-100 border border-white border-opacity-50 rounded-2xl hover:bg-green-700"
+            onClick={() => open()}
+          >
+            {address.slice(0, 4)}...{address.slice(-4)}
+          </button>
         ) : (
           <button
             onClick={() => open()}
